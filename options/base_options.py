eea84427b0e8ca17433f5e280e90b341967fb00d
commashy <96168749+commashy@users.noreply.github.com>
2025-02-28 00:59:11 +0800

    Update base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -35,7 +35,7 @@ class BaseOptions():
         parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in the last conv layer')
         parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in the first conv layer')
         parser.add_argument('--netD', type=str, default='multi', choices=['basic', 'n_layers', 'pixel', 'patch', 'multi'], help='specify discriminator architecture. The basic model is a 70x70 PatchGAN. n_layers allows you to specify the layers in the discriminator')
-        parser.add_argument('--netG', type=str, default='unet2', choices=['resnet_9blocks', 'resnet_6blocks', 'unet_256', 'unet_128', 'semcg'], help='specify generator architecture')
+        parser.add_argument('--netG', type=str, default='SemCG', choices=['resnet_9blocks', 'resnet_6blocks', 'unet_256', 'unet_128', 'semCG'], help='specify generator architecture')
         parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
         parser.add_argument('--normG', type=str, default='instance', choices=['instance', 'batch', 'none'], help='instance normalization or batch normalization for G')
         parser.add_argument('--normD', type=str, default='instance', choices=['instance', 'batch', 'none'], help='instance normalization or batch normalization for D')
